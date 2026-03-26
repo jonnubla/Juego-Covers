c97abeb1ca0e13c4138b07f7064e1b100287cb42
jonnubla <jonnubla@hotmail.com>
2026-03-26 17:45:48 +0100

    Update tracks.js

--- tracks.js
+++ tracks.js
@@ -7,22 +7,22 @@ const tracks = [
 {
 "id": 2,
 "apple": "https://music.apple.com/es/album/across-the-universe/269443984?i=269444045",
-"spotify": "https://open.spotify.com/intl-es/track/6ZQgooEk8PvsLxedYaT6Oa?si=15259d99df364f95"
+"spotify": "https://open.spotify.com/intl-es/track/6ZQgooEk8PvsLxedYaT60a?si=15259d99df364f95"
 },
 {
 "id": 3,
 "apple": "https://music.apple.com/es/album/against-all-odds/22552146?i=22552143",
-"spotify": "https://open.spotify.com/intl-es/track/1bljleDhx8PmXMYUIOVImS?si=8cfd1bb20e0748c2"
+"spotify": "https://open.spotify.com/intl-es/track/1bljleDhx8PmXMYUI0VImS?si=8cfd1bb20e0748c2"
 },
 {
 "id": 4,
 "apple": "",
-"spotify": "https://open.spotify.com/intl-es/track/0uO3bFzX9X6zUuI9h9t7kW"
+"spotify": "https://open.spotify.com/intl-es/track/0u03bFzX9X6zUuI9h9t7kw"
 },
 {
 "id": 5,
 "apple": "",
-"spotify": "https://open.spotify.com/intl-es/track/4ZtFanR9U6ndgddUvNcjcG"
+"spotify": "https://open.spotify.com/intl-es/track/4ZtFanR9U6ndgddUvNcjcg"
 },
 {
 "id": 6,
